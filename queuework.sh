596a12d1c757ce2fe3a3da51da23411b33173834
benjiroArthur <majorcaios@gmail.com>
2022-07-16 14:42:51 +0000

    dep

--- queuework.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# domain=$1
-# echo "Queue started"
-cd /var/www/asabeee.com/public_html/asabeee/ && php artisan queue:work --daemon --timeout=600 --tries=3 --delay=30 --sleep=60
-#cd /var/www/asabeee.com/public_html/asabeee/ && php artisan queue:work --daemon --timeout=600 --tries=3 --delay=30 --sleep=60 >> /dev/null 2>&1 &
-
-# cd /home/projects/ARMS-ERP
-# docker exec arms-app nohup php artisan queue:work --daemon --timeout=600 --tries=3 --delay=30 --sleep=60 >> /dev/null 2>&1 &
-
